1e4fac689349f0aed8b9dcf929848b601e489789
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-03-10 17:41:41 -0400

    conhecendo e trabalhando com listas

--- forca.py
+++ forca.py
@@ -1,9 +1,31 @@
 def jogar():
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    enforcou = False
+    acertou = False
+
     print("\n")    
     print(33 * "x")
     print("Bem vindo ao jogo de Forca!")
     print(33 * "x")
+    print()
+    print(letras_acertadas)
+    print()
+
+    while (not enforcou and not acertou):
+        chute = input("Informe uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = chute.upper()
+            index = index + 1
+        print(letras_acertadas)
+        print()
+    
+    print("Fim de jogo!")
 
 if (__name__ == "__main__"):
     jogar()
