9752d3eeb2e48a3396d59013d235cb8d46621646
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-03-12 13:10:39 -0400

    implementando o encerramento do jogo

--- forca.py
+++ forca.py
@@ -1,31 +1,53 @@
 def jogar():
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "uva".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
+    erros = 0
+    tentativas_maximas = 6
 
     print("\n")    
     print(33 * "x")
     print("Bem vindo ao jogo de Forca!")
     print(33 * "x")
-    print()
-    print(letras_acertadas)
-    print()
 
     while (not enforcou and not acertou):
+        print()
+        print(letras_acertadas)
         chute = input("Informe uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = chute.upper()
-            index = index + 1
-        print(letras_acertadas)
-        print()
-    
+
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = chute
+                index = index + 1
+        
+        else:
+            erros += 1
+            if (erros != 6):
+                print("Você errou!")
+                print("Você ainda possui {} tentativas.".format(tentativas_maximas-erros))
+
+        enforcou = erros == tentativas_maximas
+        acertou = "_" not in letras_acertadas
+
+        if(acertou):
+            print()
+            print(letras_acertadas)
+
     print("Fim de jogo!")
+    
+    if (enforcou):
+        print("Você Perdeu!")
+        print("Palavra secreta = {}".format(palavra_secreta))
+        print()
+    else:
+        print("Você Ganhou!")
+        print()
 
 if (__name__ == "__main__"):
     jogar()
